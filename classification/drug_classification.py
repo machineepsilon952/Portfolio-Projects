5c71e0c936a92fcefdf9a76f69e3d5e4895355ea
souphilt <87454247+souphilt@users.noreply.github.com>
2022-06-18 07:06:23 -0700

    Update drug_classification.py

--- classification/drug_classification.py
+++ classification/drug_classification.py
@@ -1,45 +1,41 @@
+import numpy as np
 import pandas as pd
-
+import os
+from sklearn.linear_model import LinearRegression
+from sklearn.impute import SimpleImputer
 from sklearn.preprocessing import OrdinalEncoder
-from sklearn.preprocessing import StandardScaler
-from sklearn.model_selection import train_test_split
-
 from sklearn.svm import SVC
+from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
 from sklearn.neural_network import MLPClassifier
+from sklearn.preprocessing import StandardScaler
+from sklearn.tree import DecisionTreeRegressor
 
 df = pd.read_csv("drug_classification.csv")
-
 enc = OrdinalEncoder()
+df.head()
+df.columns
 df[["Drug", "Sex", "BP", "Cholesterol"]]=enc.fit_transform(df[["Drug", "Sex", "BP", "Cholesterol"]])
-
 scaler = StandardScaler()
-df[["Age","Na_to_K"]] = scaler.fit_transform(df[["Age","Na_to_K"]])
-
-X_train, X_test, Y_train,Y_test = train_test_split(df[ ["Sex", "BP", "Cholesterol","Na_to_K"]],df["Drug"], stratify = df[["Drug"]], random_state = 10)
-
+df[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']] = scaler.fit_transform(df[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']])
+X_train, X_test, Y_train,Y_test = train_test_split(df[['Age', 'Sex', 'BP', 'Cholesterol', 'Na_to_K']],df["Drug"], stratify = df["Drug"],test_size=  .2)\
 
 lm = LogisticRegression()
 svc = SVC() 
-nnclf = MLPClassifier(solver = "lbfgs", max_iter = 200, hidden_layer_sizes = (), random_state = 10)
+nnclf = MLPClassifier(solver = "lbfgs", max_iter = 500, hidden_layer_sizes = (), random_state = 10)
+tree = DecisionTreeRegressor()
 
-print("Support vector machine scores")
+tree.fit(X_train, Y_train)
 svc.fit(X_train, Y_train)
-print("Accuracy at predicting the correct drug on training data is: ",round( 100*svc.score(X_train, Y_train),2),"%")
-print("Accuracy at predicting the correct drug on the test data is: ",round( 100*svc.score(X_test, Y_test),2),"%")
-
-
-print("Logistic Regression Scores")
 lm.fit(X_train, Y_train)
-print("Accuracy at predicting the correct drug on training data is: ",round( 100*lm.score(X_train, Y_train),2), "%")
+nnclf.fit(X_train,Y_train)
+
+svc.score(X_train, Y_train)
+svc.score(X_test,Y_test )
+lm.score(X_train, Y_train)
 lm.score(X_test,Y_test)
-print("Accuracy at predicting the correct drug on the test data is: ",round( 100*lm.score(X_test, Y_test),2), "%")
-
-
-print("MLPClassifer accuracy")
-nnclf.fit(X_train, Y_train)
-print("Accuracy at predicting the correct drug on training data is: ",round(100*nnclf.score(X_train,Y_train),2), "%")
-print("Accuracy at predicting the correct drug on the test data is: ",round( 100*nnclf.score(X_test, Y_test),2), "%")
-
-
+tree.score(X_train, Y_train)
+tree.score(X_test, Y_test)
+nnclf.score(X_train,Y_train)
+nnclf.score(X_test,Y_test)
 
