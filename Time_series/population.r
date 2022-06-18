17a6a5a418bab1467958bd7433fd6685a2c64d0d
souphilt <87454247+souphilt@users.noreply.github.com>
2022-06-18 06:46:10 -0700

    Update population.r

--- Time_series/population.r
+++ Time_series/population.r
@@ -1,4 +1,5 @@
 library(forecast)
+setwd("/home/ethan/Datasets/population")
 df = read.csv(file = 'POP.csv')
 
 pop = ts(df["value"], start = 1, frequency = 1)
@@ -10,16 +11,19 @@ pacf(pop)
 #the acf of the population data tails off really slowly since the 
 #data is not stationary
 #if we apply differencing with a lag of 1 we end up with the following acf and pacf
+plot(diff(pop))
 acf(diff(pop))
 pacf(diff(pop))
 #it appears there is a seasonal component to this data with a season length of 12
 # if we use differencing again with lag 12 we end up with the following acf and pacf plots
-
+plot(diff(diff(pop,12)))
 acf(diff(diff(pop),  12))
 pacf(diff(diff(pop), 12))
-# the acf and pacf appear to tail off after lag 1 with recuring spikes at lag 12
+# the acf and pacf indicate that this time series can be modeled using p = 1,q=1, d=1 or 2 with a seasonal component with lag 12.
 
-model <- Arima(pop_train, order = c(1,1,2), seasonal = list(order = c(0,1,1), period = 12))
+# the final model was chosen based on performance compared to several other canadate models. 
+model <- Arima(pop_train, order = c(1,1,2), seasonal = list(order = c(1,1,0), period = 12))
 autoplot(model)
 checkresiduals(model) # not normally distributed indicating the confidence intervals in the next plot are not meaningful
+# the residuals do not look normally distrubuted so the confidence intervals in the following plot are not meaningful.
 autoplot(forecast(model,h=200))+autolayer(pop)
